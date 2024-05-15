97e56dd47256d66b4c34192e5099b74c56a1069d
Mohamad Shaaban <40597819+sha3sha3@users.noreply.github.com>
2024-05-15 14:56:59 +0200

    Update top.sh

--- top.sh
+++ top.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 echo "execution started.."
 
-top -b -n 3 | sed -n '8, 12{s/^ *//;s/ *$//;s/  */,/gp;};12q' >> out.txt
-
 while [ true ]; do
-    sleep 5
     echo "Stamp Recorded.."
-    top -b -n 3 | sed -n '8, 12{s/^ *//;s/ *$//;s/  */,/gp;};12q' >> out.txt
+    top -b -n 3 | sed -n '8, 12{s/^ *//;s/ *$//;s/  */,/gp;};12q' >> top.txt
+    echo -e "---------------------------" >> top.txt
+
+    mpstat -P ALL >> mpstat.txt
+    echo -e  "---------------------------" >> mpstat.txt
+    sleep 5
 done
