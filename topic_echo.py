a4804d05192c70a978e4c64241270ebf2eca6e8a
Mohamad Shaaban <40597819+sha3sha3@users.noreply.github.com>
2024-07-25 11:13:00 +0200

    Update topic_echo.py

--- topic_echo.py
+++ topic_echo.py
@@ -4,7 +4,7 @@ import time
 import argparse, sys
 from distutils.util import strtobool
 
-topics = ['/crowd/0/joint_states','/crowd/1/joint_states','/crowd/2/joint_states','/crowd/3/joint_states','/crowd/4/joint_states']
+topics = ['/chatter','/crowd/1/joint_states','/crowd/2/joint_states','/crowd/3/joint_states','/crowd/4/joint_states']
 
 def echo_topic(topic_id):
     os.system("rostopic echo {0}".format(topic_id))
@@ -13,12 +13,12 @@ def echo_topic(topic_id):
 def main():
 
     parser=argparse.ArgumentParser()
-    parser.add_argument("--robots", type=int,help="number of robots", default=1)
+    parser.add_argument("--robots", type=int,help="number of robots", default=10)
     
     args=parser.parse_args()
     
     for x in range(args.robots):
-        topic_listener = threading.Thread(target=echo_topic, args=[topics[x]])
+        topic_listener = threading.Thread(target=echo_topic, args=[topics[0]])
         topic_listener.start()
 
 
