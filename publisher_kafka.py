490fa8b15a950cc5fff1d5ba50f8e1c49e062cf4
Mohamad Shaaban <40597819+sha3sha3@users.noreply.github.com>
2024-07-28 15:12:21 +0200

    Update publisher_kafka.py

--- publisher_kafka.py
+++ publisher_kafka.py
@@ -5,9 +5,21 @@ from std_msgs.msg import Header
 import time
 from interfaces.kafka_producer import KafkaProducer
 from interfaces.kafka_admin import KafkaAdmin
+import time, threading
 
+counter_througput=0
+
+def msg_throuput():
+    global counter_througput
+    print("Message delivered ",counter_througput)
+    counter_througput = 0
+    if rospy.is_shutdown():
+        print("shutting down...")
+    threading.Timer(1, msg_throuput).start()
+    
 def talker():
-    kafka_bootstrap_server='SASL_PLAINTEXT://172.31.35.29:9093,SASL_PLAINTEXT://172.31.35.29:9094,SASL_PLAINTEXT://172.31.35.29:9095'
+    global counter_througput
+    kafka_bootstrap_server='SASL_PLAINTEXT://172.31.35.29:9096,SASL_PLAINTEXT://172.31.35.29:9097,SASL_PLAINTEXT://172.31.35.29:9098'
     kafka_key='theengineroom'
     kafka_secret='1tYdZP43t20'
     kafka_Topic = "benchmarking_topic"
@@ -22,12 +34,13 @@ def talker():
         msg_pub.stamp = rospy.Time.now()
         msg_pub.frame_id = ""
         msg_pub.seq = 0
+        counter_througput = counter_througput +1
         kafka_producer.produce_record(topic=kafka_Topic,msg=msg_pub)
         rate.sleep()
 
 if __name__ == '__main__':
     try:
+        msg_throuput()
         talker()
     except rospy.ROSInterruptException:
         pass
-
